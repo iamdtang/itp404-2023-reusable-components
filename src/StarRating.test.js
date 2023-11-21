58df172adf83e08872e524ffd65d59051b314496
David Tang <687449+iamdtang@users.noreply.github.com>
2023-11-20 18:53:17 -0800

    Adds test for clicking on an empty star

--- src/StarRating.test.js
+++ src/StarRating.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import StarRating from "./StarRating";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
@@ -9,12 +9,7 @@ test("rendering stars", () => {
 
   // Act
   const { getAllByTestId } = render(
-    <StarRating
-      rating={rating}
-      onStarClick={(selectedRating) => {
-        setRating(selectedRating);
-      }}
-    >
+    <StarRating rating={rating} onStarClick={(selectedRating) => {}}>
       {(isStarFilled) => {
         return (
           <FontAwesomeIcon
@@ -44,13 +39,7 @@ test("rendering a specified number of stars", () => {
 
   // Act
   const { getAllByTestId } = render(
-    <StarRating
-      rating={rating}
-      total={10}
-      onStarClick={(selectedRating) => {
-        setRating(selectedRating);
-      }}
-    >
+    <StarRating rating={rating} total={10} onStarClick={(selectedRating) => {}}>
       {(isStarFilled) => {
         return (
           <FontAwesomeIcon
@@ -72,4 +61,33 @@ test("rendering a specified number of stars", () => {
 
   // Check that there is 1 empty star
   expect(getAllByTestId("empty-star").length).toBe(6);
+});
+
+test("clicking on an empty star", () => {
+  // Arrange
+  const rating = 4;
+
+  // function spy that tracks how many times it was called and with what arguments
+  const onStarClick = jest.fn();
+
+  // Act
+  const { getAllByTestId } = render(
+    <StarRating rating={rating} onStarClick={onStarClick}>
+      {(isStarFilled) => {
+        return (
+          <FontAwesomeIcon
+            icon={faStar}
+            color={isStarFilled ? "yellow" : "#eee"}
+            size="3x"
+          />
+        );
+      }}
+    </StarRating>
+  );
+
+  const firstStar = getAllByTestId("star-button")[0];
+  fireEvent.click(firstStar);
+
+  // Assert
+  expect(onStarClick).toHaveBeenCalledWith(1);
 });
